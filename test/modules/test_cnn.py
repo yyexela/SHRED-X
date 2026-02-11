07eed186fd525ec89c192d733cac2ee0031ec6b2
Alexey Yermakov <42810242+yyexela@users.noreply.github.com>
2026-02-11 14:30:57 -0800

    Added and tested pytorch_polynomial_features (#3)
    Added and tested pytorch_polynomial_features.
    
    Modified when workflows run. Lint/format/test runs on non-main branches. Documentation built on all branches, but pushed only when built in main.

--- test/modules/test_cnn.py
+++ test/modules/test_cnn.py
@@ -1,25 +1,23 @@
 """Verify the decoder modules are working correctly."""
 
 import torch
+import pytest
 from shredx.modules.cnn import CNNDecoder
 
 
-def test_cnn_decoder_forward():
+@pytest.mark.parametrize("input_size", [3, 10])
+@pytest.mark.parametrize("num_layer", [1, 3])
+def test_cnn_decoder_forward_success(input_size, num_layer):
     # Test that the CNN decoder forward pass works correctly with varying input sizes and sequence lengths
 
     batch_size = 2
-    input_sizes = 6
-    input_sizes = [3, 10]
-    num_layers = [1, 3]
     dropout = 0.1
 
     out_dim = 10
 
-    for input_size in input_sizes:
-        input_tensor = torch.randn(batch_size, 1, 1, input_size).float()
-        input_tuple = (input_tensor, None)
-        for num_layer in num_layers:
-            cnn = CNNDecoder(in_dim=input_size, out_dim=out_dim, n_layers=num_layer, dropout=dropout)
-            output = cnn(input_tuple)
-            assert output is not None
-            assert output[0].shape == (batch_size, 1, 1, out_dim)
+    input_tensor = torch.randn(batch_size, 1, 1, input_size).float()
+    input_tuple = (input_tensor, None)
+    cnn = CNNDecoder(in_dim=input_size, out_dim=out_dim, n_layers=num_layer, dropout=dropout)
+    output = cnn(input_tuple)
+    assert output is not None
+    assert output[0].shape == (batch_size, 1, 1, out_dim)
--- test/modules/test_rnn.py
+++ test/modules/test_rnn.py
@@ -1,44 +1,41 @@
 """Verify the RNN modules are working correctly."""
 
 import torch
+import pytest
 from shredx.modules.rnn import GRUEncoder, LSTMEncoder
 
 
-def test_gru_encoder_forward():
+@pytest.mark.parametrize("hidden_size", [3, 10])
+@pytest.mark.parametrize("num_layer", [1, 3])
+def test_gru_encoder_forward_success(hidden_size, num_layer):
     # Test that the GRU encoder forward pass works correctly with varying input sizes and sequence lengths
 
     batch_size = 2
     sequence_length = 10
     input_sizes = 6
-    hidden_sizes = [3, 10]
-    num_layers = [1, 3]
     dropout = 0.1
 
     input_tensor = torch.randn(batch_size, sequence_length, input_sizes).float()
 
-    for hidden_size in hidden_sizes:
-        for num_layer in num_layers:
-            gru = GRUEncoder(input_size=input_sizes, hidden_size=hidden_size, num_layers=num_layer, dropout=dropout)
-            output = gru(input_tensor)
-            assert output is not None
-            assert output[0].shape == (batch_size, 1, 1, hidden_size)
+    gru = GRUEncoder(input_size=input_sizes, hidden_size=hidden_size, num_layers=num_layer, dropout=dropout)
+    output = gru(input_tensor)
+    assert output is not None
+    assert output[0].shape == (batch_size, 1, 1, hidden_size)
 
 
-def test_lstm_encoder_forward():
+@pytest.mark.parametrize("hidden_size", [3, 10])
+@pytest.mark.parametrize("num_layer", [1, 3])
+def test_lstm_encoder_forward_success(hidden_size, num_layer):
     # Test that the LSTM encoder forward pass works correctly with varying input sizes and sequence lengths
 
     batch_size = 2
     sequence_length = 10
     input_sizes = 6
-    hidden_sizes = [3, 10]
-    num_layers = [1, 3]
     dropout = 0.1
 
     input_tensor = torch.randn(batch_size, sequence_length, input_sizes).float()
 
-    for hidden_size in hidden_sizes:
-        for num_layer in num_layers:
-            lstm = LSTMEncoder(input_size=input_sizes, hidden_size=hidden_size, num_layers=num_layer, dropout=dropout)
-            output = lstm(input_tensor)
-            assert output is not None
-            assert output[0].shape == (batch_size, 1, 1, hidden_size)
+    lstm = LSTMEncoder(input_size=input_sizes, hidden_size=hidden_size, num_layers=num_layer, dropout=dropout)
+    output = lstm(input_tensor)
+    assert output is not None
+    assert output[0].shape == (batch_size, 1, 1, hidden_size)
